07e5060392150a430a621b883aa77427e6174f62
Michael Horne <mike@recantha.co.uk>
2023-11-18 15:03:30 +0000

    Add reboot startup text file, update voyager.py with text display function and add shutdown command

---
+++ readme_about_start_on_reboot.txt
@@ -0,0 +1,1 @@
+Use the crontab method to run the script, not systemd as I couldn't get it to work!
--- voyager.py
+++ voyager.py
@@ -1,15 +1,18 @@
 from gpiozero import Button
 from signal import pause
 from pygame import mixer
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import ST7789 as ST7789
 import time
+import os
 
 button_a = Button(5)
 button_b = Button(6)
 button_x = Button(16)
 button_y = Button(24)
 file_no = -1
+folder = "/home/pi/StarTrekVoyagerPi"
+
 files = ["VoyagerMainTitle.mp3", "VoyagerCore.mp3", "VoyagerRedAlert.mp3", "VoyagerTransporter.mp3", "VoyagerWarp.mp3"]
 
 def stop_current_track():
@@ -21,7 +24,7 @@ def play_track(filename):
 
     print("Playing " + filename)
 
-    mixer.music.load(filename)
+    mixer.music.load(folder + "/" + filename)
     mixer.music.play()
     mixer.music.set_volume(0.3)
 
@@ -49,15 +52,35 @@ def prev_track():
 
     play_track(files[file_no])
 
+def display_text(the_text):
+    global disp
+
+    # Draw black square
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 18)
+    size_x, size_y = draw.textsize(the_text, font)
+    text_x = 0
+    text_y = 0
+
+    draw.text((text_x, text_y), the_text, font=font, fill=(255, 0, 0))
+    disp.display(img)
+
 def shutdown():
+    global shutting_down
+
+    shutting_down = True
+
     if button_x.is_pressed and button_y.is_pressed:
-        print("Shutdown")
+        display_text("Shutdown")
+        time.sleep(3)
         mixer.music.stop()
+        os.system('sudo shutdown now')
 
 def display_image(filename):
     global disp
 
-    image = Image.open(filename)
+    image = Image.open(folder + "/" + filename)
     disp.display(image)
 
     print("Displayed image: " + filename)
@@ -96,6 +119,8 @@ image_files = [
 ]
 image_num = -1
 
+display_text("Starting up...")
+
 # Setup button actions
 button_a.when_pressed = prev_track
 button_b.when_pressed = next_track
@@ -105,12 +130,15 @@ button_y.when_pressed = pause
 # Start initial track
 next_track()
 
+shutting_down = False
+
 while True:
     image_num = image_num + 1
     if image_num == len(image_files):
         image_num = 0
 
-    display_image(image_files[image_num])
+    if not shutting_down:
+        display_image(image_files[image_num])
 
     if not mixer.music.get_busy():
         next_track()
